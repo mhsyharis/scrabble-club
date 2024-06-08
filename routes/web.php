9812ecb7460ff0663e07d1738117a9822da523fc
mhsyharis <social.acc.muhammad@gmail.com>
2024-06-09 04:19:59 +0500

    Implemented Routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\MemberController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Home');
-});
+Route::get('/', [MemberController::class, 'leaderboard']);
+
+Route::resource('members', MemberController::class);
+Route::resource('games', GameController::class);
