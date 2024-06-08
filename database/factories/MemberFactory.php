81082a25084fb50df6e8f5afd55ca820e33c985b
mhsyharis <social.acc.muhammad@gmail.com>
2024-06-09 04:11:35 +0500

    Added Factories & Seeders

---
+++ database/factories/MemberFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Member>
+ */
+class MemberFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $this->faker->numerify(str_repeat('#', rand(9, 11))),
+            'joined_at' => $this->faker->date(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Game;
+use App\Models\Member;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +17,16 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        Member::factory(100)->create()->each(function ($member) {
+            $games = Game::factory(5)->create();
+            foreach ($games as $game) {
+                $member->games()->attach($game->id, ['score' => rand(50, 300)]);
+            }
+        });
     }
 }
